89be0acb34b5a062be5eedcf538d32a3b3841a3c
Ricsen <105400052+Ricsen12345@users.noreply.github.com>
2023-04-09 05:50:26 +0000

    Added JSON response

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def root():
   return "Hello World"
+
+@app.route('/test')
+def test():
+  return jsonify({"message": "JSON response"})
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
